82493cb4ad801613af8a826352dcd20f0860d5e7
Kristin Morris <kristinmorris@hotmail.co.uk>
2018-03-15 20:22:44 +0000

    Added Comments

---
+++ db/migrate/20180315200816_change_posts_id_in_comments.rb
@@ -0,0 +1,5 @@
+class ChangePostsIdInComments < ActiveRecord::Migration[5.1]
+  def change
+    rename_column :comments, :posts_id, :post_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315192241) do
+ActiveRecord::Schema.define(version: 20180315200816) do
 
   create_table "comments", force: :cascade do |t|
-    t.integer "posts_id"
+    t.integer "post_id"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["posts_id"], name: "index_comments_on_posts_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
