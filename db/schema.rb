ec7faef0aa1b96d499e1bd8311e4f1e5da96749a
Kristin Morris <kristinmorris@hotmail.co.uk>
2018-03-18 16:44:42 +0000

    Added Users Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315200816) do
+ActiveRecord::Schema.define(version: 20180318162514) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20180315200816) do
   create_table "posts", force: :cascade do |t|
     t.text "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
