ce8a1a3841670cd139c1ffc2148ad9df39b46a33
Kristin Morris <kristinmorris@hotmail.co.uk>
2018-03-20 18:42:44 +0000

    Add Devise Gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
-
-  validates :content, presence: true, 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  resources :posts
-  resources :comments, only: [:create, :update, :destroy]
+    resources :posts
+      resources :comments, only: [:create, :update, :destroy]
+  
+
 
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
