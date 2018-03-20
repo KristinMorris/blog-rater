3f466e7057c2e15449040e27725ec7a21d4ea79b
Kristin Morris <kristinmorris@hotmail.co.uk>
2018-03-20 18:22:27 +0000

    Adding Validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
+
+  validates :content, presence: true, length: {in: 1..360}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
+  validates :username, presence: true, length: {in: 2..70}, uniqueness: true
+  validates :email, presence: true, length: {in: 2..70}, uniqueness: true
 end
